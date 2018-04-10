e520f4461a59b459273724578f2df354aa7f908b
zhanjh <zhanjh@126.com>
2018-04-10 15:57:31 +0800

    uglifyjs support es6, es7

--- test/src/js/main.js
+++ test/src/js/main.js
@@ -1,6 +1,12 @@
 import {sum} from './lib/sum.js';
+import {divide} from './lib/divide.js';
 import {sub} from 'sub';
 
-const res = sum(1, 2) + sub(4, 3);
+const res = sum(1, 2) + sub(4, 3) + divide(5, 3);
 
 console.log(res);
+
+(async () => {
+    console.log(1);
+    console.log(2);
+})();
